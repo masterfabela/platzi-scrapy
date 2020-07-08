327ecd585c99767e40b4f0f55587c6a120ba2274
Jose Ramón Vieites Romay <fabelaretorcida@gmail.com>
2020-07-08 17:06:31 +0200

    Añadido tipo a resposta

--- hello-world/tutorial/tutorial/spiders/quotes_spider.py
+++ hello-world/tutorial/tutorial/spiders/quotes_spider.py
@@ -1,4 +1,5 @@
 import scrapy
+from scrapy.http.response.html import HtmlResponse
 
 class QuotesSpider(scrapy.Spider):
     name = 'quotes'
@@ -6,6 +7,8 @@ class QuotesSpider(scrapy.Spider):
         'http://quotes.toscrape.com/'
     ]
 
-    def parse(self, response):
+    def parse(self, response:HtmlResponse):
+        titulo = response.xpath('//h1/a/text').get()
+        citas = response.xpath('//span[@class="text" and @itemprop="text"]/text()').getall()
         with open('resultados.html', 'w', encoding='utf-8') as f:
             f.write(response.text)
